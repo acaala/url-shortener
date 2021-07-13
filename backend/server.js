0c8f304fb8c4581d00a0f1420f411944eff27e77
Liam W <elnewatts@gmail.com>
2021-07-13 11:49:36 +0000

    now creates a short url and saves to db

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const ShortUrl = require('./models/shortUrl')
 const app = express()
 
 const dbURI = process.env.DB_URI
@@ -10,12 +11,25 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection;
 db.once('open', () => console.log('Connected to DB'))
 
-app.get('/', (req, res) => {
-    res.send('Hello from server')
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+    const shortUrls = await ShortUrl.find()
+    res.json({ shortUrls })
 })
 
-app.post('/shortUrls', (req, res) => {
+app.post('/shortUrls', async (req, res) => {
+    console.log({ full: req.body.fullUrl })
+    await ShortUrl.create({ full: req.body.fullUrl })
+    res.json({ redirect: '/'})
+})
 
+app.get('/:shortUrl', async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({short: req.params.shortUrl })
+  
+  if(shortUrl == null ) return res.sendStatus(404)
+
+  // res.json({ redirect: shortUrl.full })
 })
 
 app.listen(process.env.PORT || 5000)
