2cef271fe76d874f64f4ea5f640c8a404cafeeda
Liam W <elnewatts@gmail.com>
2021-07-13 11:49:02 +0000

    Added bootstrap and css for frontend created url card

---
+++ frontend/src/lib/UrlCard.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let _id;
+	export let short;
+	export let full;
+</script>
+
+<div class="card mb-2">
+	<div class="card-header">url id: {_id}</div>
+	<div class="card-body">
+		<h5 class="card-title">{short}</h5>
+		<p class="card-text">
+			{full}
+		</p>
+		<a href="/" class="btn btn-primary">Go</a>
+	</div>
+</div>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,3 +1,50 @@
-<h1>Url Shortner</h1>
-<input type="text" />
-<button>Submit</button>
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import UrlCard from '$lib/UrlCard.svelte';
+
+	let fullUrl: string;
+	let urls = [];
+	const url = 'http://localhost:5000/shortUrls';
+
+	const handleSubmit = () => {
+		const options = {
+			method: 'POST',
+			body: JSON.stringify({ fullUrl }),
+			headers: {
+				'Content-type': 'application/json'
+			}
+		};
+
+		fetch(url, options)
+			.then((res) => res.json())
+			.then((res) => (window.location.href = res.redirect));
+	};
+
+	async function fetchAllUrls() {
+		await fetch('http://localhost:5000/')
+			.then((r) => r.json())
+			.then((data) => (urls = data.shortUrls));
+		console.log(urls);
+	}
+
+	onMount(fetchAllUrls);
+</script>
+
+<div class="container text-center mt-5">
+	<h1 class="display-6">Url Shortner</h1>
+	<div class="d-flex justify-content-center">
+		<input placeholder="Url" name="fullUrl" class="form-control" type="url" bind:value={fullUrl} />
+
+		<button class="btn btn-primary" on:click={handleSubmit}>Submit</button>
+	</div>
+</div>
+
+<div class="container mt-5">
+	{#if urls.length > 0}
+		{#each urls as url}
+			<UrlCard {...url} />
+		{/each}
+	{:else}
+		<p>No urls yet..</p>
+	{/if}
+</div>
