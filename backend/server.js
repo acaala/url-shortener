3c0383553e93f9f77c5ee0924e0ee7c7b85103c6
Liam W <elnewatts@gmail.com>
2021-07-18 10:56:21 +0000

    corrected typo

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
-const corse = require('cors')
+const cors = require('cors')
 const urlRoutes = require('./routes/urlRoutes')
 const app = express()
 
