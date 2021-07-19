9cd3525ea4a4d45e51c02f4a43d4897d0c7e01a9
Liam W <elnewatts@gmail.com>
2021-07-19 14:24:52 +0000

    removed delete button untill auth implimented

--- frontend/src/lib/UrlCard.svelte
+++ frontend/src/lib/UrlCard.svelte
@@ -13,14 +13,14 @@
 	};
 </script>
 
-<div class="card mb-2">
-	<div class="card-header">url id: {_id}</div>
+<div class="card mb-3 shadow">
+	<a class="card-header" href="https://coast-url.netlify.app/{short}">https://coast-url.netlify.app/{short}</a>
 	<div class="card-body">
 		<h5 class="card-title">{short}</h5>
 		<p class="card-text">
 			{full}
 		</p>
 		<a href={`/${short}`} class="btn btn-primary">Go</a>
-		<button on:click={handleDelete} class="btn btn-danger">Delete</button>
+		<!-- <button on:click={handleDelete} class="btn btn-danger">Delete</button> -->
 	</div>
 </div>
